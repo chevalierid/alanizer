83e17245b7c4f0852b0ce7dcf5602af5b4684d15
Daphne Chevalier <16675899+chevalierid@users.noreply.github.com>
2026-01-30 15:40:47 -0800

    Add files via upload

---
+++ Utilities/get_time_v3.py
@@ -0,0 +1,21 @@
+# Importing Libraries
+import serial
+import time
+rtc_set = 0
+
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
+def write_read(x):
+    print(x) # printing the value
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+while rtc_set == 0:
+    time.sleep(5)
+    print(str(int(time.time())))
+    value = write_read(str(int(time.time()))).decode()
+    print(value) # printing the value
+    if value == "set\r\n":
+        arduino.close()
+        rtc_set = 1
+
